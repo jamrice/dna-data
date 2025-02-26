5e0a5419fb4133718f837ad71cb8e6b8a1fbb4fc
tomato <ahsdfg30@gmail.com>
2025-02-26 22:21:24 +0900

    Feat: Seperate consine similarity calculation of summary and other contents

--- src/experiments/run_bill_recommendation_english.py
+++ src/experiments/run_bill_recommendation_english.py
@@ -30,21 +30,38 @@ class SimilarityScoreGenerator:
 
             # 각 법안에 대해 요약을 영어로 번역
             for bill in self.contents:
-                summarizer = Summarizer(bill["bill_content"])  # bill_content를 사용
-                translated_content = summarizer.translate_to_english()
+                summarizer = Summarizer(bill["bill_summary"])  # bill_content를 사용
+                translated_summary = summarizer.translate_to_english(
+                    bill["bill_summary"]
+                )
+                translated_title = summarizer.translate_to_english(
+                    bill["bill_title"]
+                )  # 제목 번역 추가
 
                 # 번역된 요약이 None이 아닌 경우에만 추가
-                if translated_content is not None:
+                if translated_summary is not None and translated_title is not None:
                     translated_contents.append(
-                        {"id": bill["bill_id"], "content": translated_content}
+                        {
+                            "id": bill["bill_id"],
+                            "translated_bill_title": translated_title,
+                            "translated_bill_summary": translated_summary,
+                        }
                     )
                     logger.info(
-                        f"Translated content for bill {bill['bill_id']}: {translated_content}"
+                        f"Translated content for bill {bill['bill_id']}: \n {translated_title} \n {translated_summary}"
                     )
                 else:
+                    # 애초에 요약이 없는 문서들을 어떻게 처리할지 이야기 필요함
+                    translated_contents.append(
+                        {
+                            "id": bill["bill_id"],
+                            "translated_bill_title": bill["bill_title"],
+                            "translated_bill_summary": "No Content",
+                        }
+                    )
                     logger.warning(f"Translation failed for bill {bill['bill_id']}")
 
-            # translated_contents 가 없을 경우 return
+            # translated_summaries 가 없을 경우 return
             if not translated_contents:
                 logger.warning("No valid translated contents found.")
                 return
@@ -57,13 +74,45 @@ class SimilarityScoreGenerator:
             logger.error(f"Error in translate_content process: {str(e)}")
             raise
 
-    def generate_similarity_score(self):
-        """모든 법안에 대한 추천 생성"""
+    def generate_summary_similarity_score(self):
+        """summary에 대한 similarity score 생성"""
         try:
             # TF-IDF 벡터라이저 설정
             tfidf_vectorizer = TfidfVectorizer(stop_words="english")
             tfidf_matrix = tfidf_vectorizer.fit_transform(
-                [bill["content"] for bill in self.translated_contents]
+                [bill["translated_bill_summary"] for bill in self.translated_contents]
+            )
+
+            # 코사인 유사도 계산
+            cosine_sim = cosine_similarity(tfidf_matrix)
+            cosine_sim_df = pd.DataFrame(cosine_sim)
+
+            # 추천 결과 생성 및 DB에 저장
+            for idx, bill in enumerate(self.translated_contents):
+                source_bill_id = bill["id"]  # 현재 법안의 ID를 source_bill_id로 설정
+                for sim_idx in range(
+                    len(self.translated_contents)
+                ):  # 모든 법안에 대해 반복
+                    if sim_idx != idx:  # 자기 자신 제외
+                        target_bill_id = self.translated_contents[sim_idx]["id"]
+                        similarity_score = cosine_sim[idx][sim_idx]
+                        self.db_handler.save_similarity_score(
+                            target_bill_id, source_bill_id, similarity_score
+                        )
+
+            return cosine_sim_df
+
+        except Exception as e:
+            logger.error(f"Error in recommendation generation process: {str(e)}")
+            raise
+
+    def generate_others_similarity_score(self):
+        """summary 제외 내용들에 대한 similarity score 생성"""
+        try:
+            # TF-IDF 벡터라이저 설정
+            tfidf_vectorizer = TfidfVectorizer(stop_words="english")
+            tfidf_matrix = tfidf_vectorizer.fit_transform(
+                [bill["translated_bill_title"] for bill in self.translated_contents]
             )
 
             # 코사인 유사도 계산
@@ -96,5 +145,7 @@ if __name__ == "__main__":
     print("contents: ", contents)
     t_contents = ssg.translate_content()
     print("t_contents: ", t_contents)
-    cosin_sim_df = ssg.generate_similarity_score()
-    print(cosin_sim_df)
+    summary_cosine_sim_df = ssg.generate_summary_similarity_score()
+    print("summary df:\n", summary_cosine_sim_df)
+    other_cosine_sim_df = ssg.generate_others_similarity_score()
+    print("other df:\n", other_cosine_sim_df)
