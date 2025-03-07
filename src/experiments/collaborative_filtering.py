3312716e378cbc3602f7e23672e5a39f461b651d
tomato <ahsdfg30@gmail.com>
2025-03-07 23:08:10 +0900

    WIP: WIP with test code

--- src/experiments/collaborative_filtering.py
+++ src/experiments/collaborative_filtering.py
@@ -15,30 +15,30 @@ class SimilarityScoreGenerator:
 
     def generate_similarity_score(self):
         # Retrieve the user-bill score data from the database
-        ratings = (
-            self.db_handler.get_metric_data()
-        )  # Assuming this returns a DataFrame with 'User-ID', 'Bill-Title', and 'user_score'
+        metrics = self.db_handler.execute_query("SELECT * FROM user_content_metrics")
 
         # Create the user-item matrix
-        user_item_matrix = ratings.pivot_table(
-            index="User-ID", columns="Bill-Title", values="user_score"
+        self.user_item_matrix = metrics.pivot_table(
+            index="user_id", columns="content_id", values="similarity_score"
         ).fillna(0)
 
         # Compute the cosine similarity matrix
-        return cosine_similarity(user_item_matrix)
+        return cosine_similarity(self.user_item_matrix)
 
     def collaborative_filtering_recommendation(self, user_id, k=10):
         user_similarities = self.cosine_sim_matrix[user_id]
         top_k_similar_users = np.argsort(user_similarities)[-k - 1 : -1][::-1]
 
-        # Retrieve user scores instead of ratings
-        similar_users_scores = [
-            self.db_handler.get_metric(user) for user in top_k_similar_users
-        ]
+        # Retrieve user scores from the user-item matrix
+        similar_users_scores = self.user_item_matrix.iloc[top_k_similar_users]
 
         # Calculate mean scores
-        mean_scores = np.mean(similar_users_scores, axis=0)
+        mean_scores = similar_users_scores.mean(axis=0)
 
         # Get top k books based on mean scores
         top_k_bills = mean_scores.sort_values(ascending=False).head(k).index
         return top_k_bills
+
+
+if __name__ == "__main__":
+    pass
