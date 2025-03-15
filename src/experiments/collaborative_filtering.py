520d9e1a73cc5c46321d1f622353c33bdc884694
tomato <ahsdfg30@gmail.com>
2025-03-15 21:14:00 +0900

    Feat: Add cf knn bias sig algorithm

--- src/experiments/collaborative_filtering.py
+++ src/experiments/collaborative_filtering.py
@@ -17,6 +17,7 @@ class CollaborativeFiltering:
         )
         self.user_cosine_similarity = self.calculate_cosine_similarity()
         self.x_train, self.x_test, self.y_train, self.y_test = self._train_test_split()
+        self.sig_level, self.min_ratings = self.set_sig_level_min_ratings()
 
     def get_metrics(self):
         metrics = self.db_handler.db.execute(text("SELECT * FROM user_content_metrics"))
@@ -47,15 +48,15 @@ class CollaborativeFiltering:
     def RMSE(self, y_true, y_pred):
         return np.sqrt(np.mean((np.array(y_true) - np.array(y_pred)) ** 2))
 
-    def score(self, model):
+    def score(self, model: function):
         id_pairs = zip(self.x_test["user_id"], self.x_test["content_id"])
-        y_pred = np.array([model(user, movie) for (user, movie) in id_pairs])
+        y_pred = np.array([model(user, content) for (user, content) in id_pairs])
         y_true = np.array(self.x_test["metric_score"])
         return self.RMSE(y_true, y_pred)
 
     # 주어진 컨텐츠의 (content_id) 가중평균 rating을 계산하는 함수,
     # 가중치는 주어진 사용자와 다른 사용자 간의 유사도(user_similarity)
-    def cf_simple(self, user_id, content_id):
+    def cf_simple(self, user_id: int, content_id: str):
         if content_id in self.full_matrix:
             # 현재 사용자와 다른 사용자 간의 similarity 가져오기
             sim_scores = self.user_cosine_similarity[user_id].copy()
@@ -74,22 +75,22 @@ class CollaborativeFiltering:
         return mean_rating
 
     # Neighbor size를 정해서 예측치를 계산하는 함수
-    def cf_knn(self, user_id, movie_id, neighbor_size=2):
-        if movie_id in self.full_matrix:
+    def cf_knn(self, user_id: int, content_id: str, neighbor_size=2):
+        if content_id in self.full_matrix:
             # 현재 사용자와 다른 사용자 간의 similarity 가져오기
             sim_scores = self.user_cosine_similarity[user_id].copy()
             # 현재 컨텐츠에 대한 모든 사용자의 rating값 가져오기
-            movie_ratings = self.full_matrix[movie_id].copy()
+            content_ratings = self.full_matrix[content_id].copy()
             # 현재 컨텐츠를 평가하지 않은 사용자의 index 가져오기
-            none_rating_idx = movie_ratings[movie_ratings.isnull()].index
+            none_rating_idx = content_ratings[content_ratings.isnull()].index
             # 현재 컨텐츠를 평가하지 않은 사용자의 rating (null) 제거
-            movie_ratings = movie_ratings.drop(none_rating_idx)
+            content_ratings = content_ratings.drop(none_rating_idx)
             # 현재 컨텐츠를 평가하지 않은 사용자의 similarity값 제거
             sim_scores = sim_scores.drop(none_rating_idx)
             ##### Neighbor size가 지정되지 않은 경우
             if neighbor_size == 0:
                 # 현재 컨텐츠를 평가한 모든 사용자의 가중평균값 구하기
-                mean_rating = np.dot(sim_scores, movie_ratings) / sim_scores.sum()
+                mean_rating = np.dot(sim_scores, content_ratings) / sim_scores.sum()
             ##### Neighbor size가 지정된 경우
             else:
                 # 해당 컨텐츠를 평가한 사용자가 최소 2명이 되는 경우에만 계산
@@ -98,56 +99,56 @@ class CollaborativeFiltering:
                     neighbor_size = min(neighbor_size, len(sim_scores))
                     # array로 바꾸기 (argsort를 사용하기 위함)
                     sim_scores = np.array(sim_scores)
-                    movie_ratings = np.array(movie_ratings)
+                    content_ratings = np.array(content_ratings)
                     # 유사도를 순서대로 정렬
                     user_idx = np.argsort(sim_scores)
                     # 유사도를 neighbor size만큼 받기
                     sim_scores = sim_scores[user_idx][-neighbor_size:]
                     # 컨텐츠 rating을 neighbor size만큼 받기
-                    movie_ratings = movie_ratings[user_idx][-neighbor_size:]
+                    content_ratings = content_ratings[user_idx][-neighbor_size:]
                     # 최종 예측값 계산
-                    mean_rating = np.dot(sim_scores, movie_ratings) / sim_scores.sum()
+                    mean_rating = np.dot(sim_scores, content_ratings) / sim_scores.sum()
                 else:
                     mean_rating = 5.0
         else:
             mean_rating = 5.0
         return mean_rating
 
-    def cf_knn_bias(self, user_id, movie_id, neighbor_size=0):
-        # train 데이터의 user의 rating 평균과 영화의 평점편차 계산
+    def cf_knn_bias(self, user_id: int, content_id: str, neighbor_size=0):
+        # train 데이터의 user의 rating 평균과 컨텐츠의 평점편차 계산
         rating_mean = self.full_matrix.mean(axis=1)
         rating_bias = (self.full_matrix.T - rating_mean).T
-        if movie_id in rating_bias:
+        if content_id in rating_bias:
             # 현 user와 다른 사용자 간의 유사도 가져오기
             sim_scores = self.user_cosine_similarity[user_id].copy()
-            # 현 movie의 평점편차 가져오기
-            movie_ratings = rating_bias[movie_id].copy()
-            # 현 movie에 대한 rating이 없는 사용자 삭제
-            none_rating_idx = movie_ratings[movie_ratings.isnull()].index
-            movie_ratings = movie_ratings.drop(none_rating_idx)
+            # 현 content의 평점편차 가져오기
+            content_ratings = rating_bias[content_id].copy()
+            # 현 content에 대한 rating이 없는 사용자 삭제
+            none_rating_idx = content_ratings[content_ratings.isnull()].index
+            content_ratings = content_ratings.drop(none_rating_idx)
             sim_scores = sim_scores.drop(none_rating_idx)
             ##### (2) Neighbor size가 지정되지 않은 경우
             if neighbor_size == 0:
                 # 편차로 예측값(편차 예측값) 계산
-                prediction = np.dot(sim_scores, movie_ratings) / sim_scores.sum()
+                prediction = np.dot(sim_scores, content_ratings) / sim_scores.sum()
                 # 편차 예측값에 현 사용자의 평균 더하기
                 prediction = prediction + rating_mean[user_id]
             ##### (3) Neighbor size가 지정된 경우
             else:
-                # 해당 영화를 평가한 사용자가 최소 2명이 되는 경우에만 계산
+                # 해당 컨텐츠를 평가한 사용자가 최소 2명이 되는 경우에만 계산
                 if len(sim_scores) > 1:
-                    # 지정된 neighbor size 값과 해당 영화를 평가한 총사용자 수 중 작은 것으로 결정
+                    # 지정된 neighbor size 값과 해당 컨텐츠를 평가한 총사용자 수 중 작은 것으로 결정
                     neighbor_size = min(neighbor_size, len(sim_scores))
                     # array로 바꾸기 (argsort를 사용하기 위함)
                     sim_scores = np.array(sim_scores)
-                    movie_ratings = np.array(movie_ratings)
+                    content_ratings = np.array(content_ratings)
                     # 유사도를 순서대로 정렬
                     user_idx = np.argsort(sim_scores)
                     # 유사도와 rating을 neighbor size만큼 받기
                     sim_scores = sim_scores[user_idx][-neighbor_size:]
-                    movie_ratings = movie_ratings[user_idx][-neighbor_size:]
+                    content_ratings = content_ratings[user_idx][-neighbor_size:]
                     # 편차로 예측치 계산
-                    prediction = np.dot(sim_scores, movie_ratings) / sim_scores.sum()
+                    prediction = np.dot(sim_scores, content_ratings) / sim_scores.sum()
                     # 예측값에 현 사용자의 평균 더하기
                     prediction = prediction + rating_mean[user_id]
                 else:
@@ -156,7 +157,70 @@ class CollaborativeFiltering:
             prediction = rating_mean[user_id]
         return prediction
 
-    def recommender(self, model, user_id, n_items=2, neighbor_size=2):
+    def set_sig_level_min_ratings(self, sig_level: int = 3, min_ratings: int = 2):
+        return sig_level, min_ratings
+
+    def cf_knn_bias_sig(self, user_id: int, content_id: str, neighbor_size=0):
+        # train 데이터의 user의 rating 평균과 컨텐츠의 평점편차 계산
+        rating_mean = self.full_matrix.mean(axis=1)
+        rating_bias = (self.full_matrix.T - rating_mean).T
+
+        # 사용자별 공통 평가 수 계산
+        rating_binary1 = np.array((self.full_matrix > 0).astype(float))
+        rating_binary2 = rating_binary1.T
+        counts = np.dot(rating_binary1, rating_binary2)
+        counts = pd.DataFrame(
+            counts, index=self.full_matrix.index, columns=self.full_matrix.index
+        ).fillna(0)
+
+        if content_id in rating_bias:
+            # 현 user와 다른 사용자 간의 유사도 가져오기
+            sim_scores = self.user_cosine_similarity[user_id]
+            # 현 content의 평점편차 가져오기
+            content_ratings = rating_bias[content_id]
+            # 현 content에 대한 rating이 없는 사용자 표시
+            no_rating = content_ratings.isnull()
+            # 현 사용자와 다른 사용자간 공통 평가 아이템 수 가져오기
+            common_counts = counts[user_id]
+            # 공통으로 평가한 컨텐츠의 수가 SIG_LEVEL보다 낮은 사용자 표시
+            low_significance = common_counts < self.sig_level
+            # 평가를 안 하였거나, SIG_LEVEL이 기준 이하인 user 제거
+            none_rating_idx = content_ratings[no_rating | low_significance].index
+            content_ratings = content_ratings.drop(none_rating_idx)
+            sim_scores = sim_scores.drop(none_rating_idx)
+            ##### (2) Neighbor size가 지정되지 않은 경우
+            if neighbor_size == 0:
+                # 편차로 예측값(편차 예측값) 계산
+                prediction = np.dot(sim_scores, content_ratings) / sim_scores.sum()
+                # 편차 예측값에 현 사용자의 평균 더하기
+                prediction = prediction + rating_mean[user_id]
+            ##### (3) Neighbor size가 지정된 경우
+            else:
+                # 해당 컨텐츠를 평가한 사용자가 최소 MIN_RATINGS 이상인 경우에만 계산
+                if len(sim_scores) > self.min_ratings:
+                    # 지정된 neighbor size 값과 해당 컨텐츠를 평가한 총사용자 수 중 작은 것으로 결정
+                    neighbor_size = min(neighbor_size, len(sim_scores))
+                    # array로 바꾸기 (argsort를 사용하기 위함)
+                    sim_scores = np.array(sim_scores)
+                    content_ratings = np.array(content_ratings)
+                    # 유사도를 순서대로 정렬
+                    user_idx = np.argsort(sim_scores)
+                    # 유사도와 rating을 neighbor size만큼 받기
+                    sim_scores = sim_scores[user_idx][-neighbor_size:]
+                    content_ratings = content_ratings[user_idx][-neighbor_size:]
+                    # 편차로 예측치 계산
+                    prediction = np.dot(sim_scores, content_ratings) / sim_scores.sum()
+                    # 예측값에 현 사용자의 평균 더하기
+                    prediction = prediction + rating_mean[user_id]
+                else:
+                    prediction = rating_mean[user_id]
+        else:
+            prediction = rating_mean[user_id]
+        return prediction
+
+    def recommender(
+        self, model: function, user_id: int, n_items: int = 2, neighbor_size: int = 2
+    ):
         """Recommend n_items for a given user based on collaborative filtering model."""
         # Calculate predicted ratings for all items for the current user
         predictions = []
@@ -191,5 +255,7 @@ if __name__ == "__main__":
     print("CF simple:", cf.score(cf.cf_simple))
     print("CF KNN:", cf.score(cf.cf_knn))
     print("CF KNN user bias:", cf.score(cf.cf_knn_bias))
+    print("CF KNN bias sig:", cf.score(cf.cf_knn_bias_sig))  # data가 없어서 아직 실험 X
     print(cf.recommender(cf.cf_knn, 1))
     print(cf.recommender(cf.cf_knn_bias, 1))
+    print(cf.recommender(cf.cf_knn_bias_sig, 1))  # data가 없어서 아직 실험 X
