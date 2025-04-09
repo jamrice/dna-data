24ea12c2e8e242aaa7c06d255d8708bb78bee505
tomato <ahsdfg30@gmail.com>
2025-04-09 21:54:05 +0900

    Feat: Change algorithm to use contents table's view column

--- src/recommendation_models/best_seller.py
+++ src/recommendation_models/best_seller.py
@@ -11,7 +11,7 @@ class BestSeller:
         self.best_sellers = self.get_best_sellers()
 
     def get_metrics(self):
-        metrics = self.db_handler.db.execute(text("SELECT * FROM user_content_metrics"))
+        metrics = self.db_handler.db.execute(text("SELECT * FROM contents"))
         metrics = (
             pd.DataFrame(metrics, columns=metrics.keys())
             .drop_duplicates(subset="id")
@@ -19,22 +19,15 @@ class BestSeller:
         )
         return metrics
 
-
     def get_best_sellers(self, top_n: int = 10) -> list:
         """
         content_id 기준으로 평균 metric_score를 계산하여
         상위 top_n개의 베스트셀러의 content_id 리스트를 반환한다.
         """
-        grouped = (
-            self.metrics.groupby("content_id")["metric_score"]
-            .mean()
-            .reset_index()
-            .rename(columns={"metric_score": "avg_metric_score"})
-            .sort_values(by="avg_metric_score", ascending=False)
-        )
+        grouped = self.metrics.sort_values(by="views", ascending=False)
         return grouped["content_id"].head(top_n).tolist()
 
 
 if __name__ == "__main__":
     bs = BestSeller()
-    print(bs.best_sellers)
+    print(bs.get_best_sellers(6))
